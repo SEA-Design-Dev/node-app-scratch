f5f228495c52027675c1bbe61c0c4aa10b8472a2
Benjamin Hrach <benjamin.hrach@gmail.com>
2014-10-01 10:51:39 -0700

    added .gitignore and minor changes to index.js and index.jade

--- Ben-Jeni-Angela/awesome_app/routes/index.js
+++ Ben-Jeni-Angela/awesome_app/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AWESOME!',
+												names: 'Ben Angela and Jeni!',
+												say: 'ROCK!'});
 });
 
 module.exports = router;
